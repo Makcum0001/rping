c5d5d93f7a5adf30a4306454bc4c36cefc7c08a7
Maxim Novikov <maksim.novikov@osinit.com>
2026-09-02 21:35:00 +0300

    feat: add generate_payload fn
    added generate_payload fn. Inside it added payload generation with
    timestamp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+
+fn generate_payload() -> [u8; 40] {
+    let mut payload: [u8; 40] = [0; 40];
+    
+    let now = SystemTime::now();
+    let timestamp_ms: u64 = now
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_millis() as u64;
+
+    payload[..8].copy_from_slice(&timestamp_ms.to_be_bytes());
+
+    payload
+}
+
 pub struct IcmpPacket<'a> {
     pub header: IcmpHeader,
     pub payload: &'a [u8; 40],
