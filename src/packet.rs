ce62d834f56e615447df548375d5fc1556d2efcf
Maxim Novikov <maksim.novikov@osinit.com>
2026-09-18 23:30:55 +0300

    refactor(packet): simplify packet module for better encapsulation
    add IcmpType to provide type of icmp packet
    move IcmpHeader instantiation to IcmpPacket

--- src/packet.rs
+++ src/packet.rs
@@ -1,7 +1,39 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use crate::checksum;
-pub struct IcmpHeader {
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum IcmpType {
+    EchoRequest,
+    EchoReply { sequence: u16 },
+    // Легко расширить в будущем:
+    // DestinationUnreachable { code: u8 },
+    // TimeExceeded,
+}
+
+impl IcmpType {
+    fn message_type(&self) -> u8 {
+        match self {
+            Self::EchoReply { .. } => 0,
+            Self::EchoRequest => 8,
+        }
+    }
+
+    fn code(&self) -> u8 {
+        match self {
+            Self::EchoRequest | Self::EchoReply { .. } => 0,
+        }
+    }
+
+    fn sequence(&self) -> u16 {
+        match self {
+            Self::EchoRequest => 0,
+            Self::EchoReply { sequence } => *sequence,
+        }
+    }
+}
+
+struct IcmpHeader {
     message_type: u8,
     code: u8,
     checksum: u16,
@@ -17,61 +49,50 @@ impl IcmpHeader {
     const ICMP_IDENTIFIER_OFFSET: usize = 4;
     const ICMP_SEQUENCE_OFFSET: usize = 6;
 
-    pub fn echo_request(id: u16) -> Self {
-        Self {
-            message_type: 8,
-            code: 0,
-            checksum: 0,
-            identifier: id,
-            sequence: 0,
-        }
-    }
-
-    pub fn echo_reply(id: u16, seq: u16) -> Self {
-        Self {
-            message_type: 0,
-            code: 0,
-            checksum: 0,
-            identifier: id,
-            sequence: seq,
-        }
-    }
-
-    pub fn to_bytes(&self) -> [u8; Self::ICMP_HEADER_SIZE] {
+    fn to_bytes(&self) -> [u8; Self::ICMP_HEADER_SIZE] {
         let mut bytes = [0u8; Self::ICMP_HEADER_SIZE];
-
-        let checksum_bytes = self.checksum.to_be_bytes();
-        let identifier_bytes = self.identifier.to_be_bytes();
-        let sequence_bytes = self.sequence.to_be_bytes();
 
         bytes[Self::ICMP_TYPE_OFFSET] = self.message_type;
         bytes[Self::ICMP_CODE_OFFSET] = self.code;
 
         bytes[Self::ICMP_CHECKSUM_OFFSET..Self::ICMP_IDENTIFIER_OFFSET]
-            .copy_from_slice(&checksum_bytes);
-
+            .copy_from_slice(&self.checksum.to_be_bytes());
         bytes[Self::ICMP_IDENTIFIER_OFFSET..Self::ICMP_SEQUENCE_OFFSET]
-            .copy_from_slice(&identifier_bytes);
-
-        bytes[Self::ICMP_SEQUENCE_OFFSET..Self::ICMP_HEADER_SIZE].copy_from_slice(&sequence_bytes);
+            .copy_from_slice(&self.identifier.to_be_bytes());
+        bytes[Self::ICMP_SEQUENCE_OFFSET..Self::ICMP_HEADER_SIZE]
+            .copy_from_slice(&self.sequence.to_be_bytes());
 
         bytes
     }
 }
 
 pub struct IcmpPacket {
-    pub header: IcmpHeader,
-    pub payload: [u8; 8],
+    header: IcmpHeader,
+    payload: [u8; 8],
 }
 
 impl IcmpPacket {
-    pub fn new(mut header: IcmpHeader) -> Self {
-        header.checksum = 0; // Для расчета чексуммы всегда обнуляем это поле
+    pub fn new(icmp_type: IcmpType, id: u16) -> Self {
+        let header = IcmpHeader {
+            message_type: icmp_type.message_type(),
+            code: icmp_type.code(),
+            checksum: 0,
+            identifier: id,
+            sequence: icmp_type.sequence(),
+        };
 
         let payload = current_timestamp_ms().to_be_bytes();
 
-        IcmpPacket { header, payload }
+        let mut packet = IcmpPacket { header, payload };
+
+        packet.update_checksum();
+
+        packet
     }
+    pub fn payload(&self) -> &[u8] {
+        &self.payload
+    }
+
     pub fn to_bytes(&self) -> [u8; 16] {
         let mut bytes = [0u8; 16];
         let header_bytes = self.header.to_bytes();
@@ -82,6 +103,7 @@ impl IcmpPacket {
 
         bytes
     }
+
     pub fn update_checksum(&mut self) {
         self.header.checksum = 0;
 
