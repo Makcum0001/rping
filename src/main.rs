bb8a489dacc10d3d2a6a7e10e025d6fde631cd4c
Maxim Novikov <maksim.novikov@osinit.com>
2026-09-19 00:48:11 +0300

    WIP: add logic for send and recieve packets

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,39 @@
 mod checksum;
 mod packet;
 
-use std::{error::Error, process};
-use socket2::{Domain, Protocol, Socket, Type};
+use packet::{IcmpPacket, IcmpType};
+use socket2::{Domain, Protocol, SockAddr, Socket, Type, MaybeUninitSlice};
+use std::mem::{self, MaybeUninit};
 use std::net::SocketAddr;
-use packet::{IcmpHeader, IcmpPacket};
+use std::{error::Error, process};
+
+const IPV4_HEADER_SIZE: usize = 20;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let id = process::id() as u16;
 
-    let icmp_header = IcmpHeader::echo_request(id);
-    let mut packet = IcmpPacket::new(icmp_header);
-    packet.update_checksum();
+    let packet = IcmpPacket::new(IcmpType::EchoRequest, id);
 
     let buf = packet.to_bytes();
 
-    let addr = SocketAddr::
-    
+    let addr: SocketAddr = "8.8.8.8:0".parse().unwrap();
+    let host = addr.ip();
+
     let socket = Socket::new(Domain::IPV4, Type::RAW, Some(Protocol::ICMPV4))?;
 
-    socket.send_to(&buf, addr)
+    println!(
+        "PING {} ({}) {}({}) bytes of data.",
+        addr.ip(),
+        host,
+        packet.payload().len(),
+        buf.len() + IPV4_HEADER_SIZE
+    );
+    let _ = socket.send_to(&buf, &SockAddr::from(addr));
+
+    let mut recv_buf: [MaybeUninit<u8>; 1024] = [MaybeUninit::uninit(); 1024];
+    let bytes_count = socket.recv(&mut recv_buf).unwrap();
+    
+    println!("Байт: {}\nБайты:{:02X?}", bytes_count, recv_buf);
+    
     Ok(())
 }
